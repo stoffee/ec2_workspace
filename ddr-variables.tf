3444385cff7e7319ed3e60623ac4bd677d242f18
stoffee <chris@roadkill.org>
2024-09-30 14:14:43 -0700

    HC Demo: Add ddr-variables.tf

---
+++ ddr-variables.tf
@@ -0,0 +1,15 @@
+
+variable "ddr_tfc_organization" {
+  type    = string
+  default = null
+}
+
+variable "ddr_user_project_id" {
+  type    = string
+  default = null
+}
+
+variable "ddr_tfc_project_name" {
+  type    = string
+  default = null
+}
